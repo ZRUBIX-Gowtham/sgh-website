5e2d3c73bc89a9d55be07d93742cfd5d10bf271f
ZRUBIX-Gowtham <gowtham@zrubix.com>
2025-09-08 19:45:58 +0530

    whatsapp model2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-import { HelmetProvider } from 'react-helmet-async';
-import ChatWidget from './chatbot/ChatWidget';
+import ChatWidget from './chatbot/page';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
